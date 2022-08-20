b57606d5f4529648019cd1cf69639ee0af4d8485
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2022-08-20 23:06:50 +0300

    v1.0

--- script.js
+++ script.js
@@ -4,41 +4,43 @@ const clear_btn_dom = document.getElementById("clear_btn");
 let all_cells_nodelist = document.getElementsByClassName("cell");
 let all_cells_array = Array.from(all_cells_nodelist);
 
+let win_mousedown = false;
+document.body.onmousedown = () => (win_mousedown = true);
+document.body.onmouseup = () => (win_mousedown = false);
+
 size_of_pad = 0;
 let container_width_js = container_dom.clientHeight;
 
 //--------------------Helper functions------------------
-function get_size_of_container() {
-  do {
-    var temp_cell = document.querySelector(".cell");
-    while (container_dom.firstChild) {
-      temp_cell = document.querySelector(".cell");
-      container_dom.removeChild(temp_cell);
-    }
-    size_of_pad = prompt("How many tiles? Insert 8 or 16");
-  } while (size_of_pad !== "8" && size_of_pad != "16");
-  return size_of_pad;
-}
+
 function clear() {
-  all_cells_nodelist.forEach((cell) => {
-    cell.style.backgroundColor = "white";
+  console.log("clear clicked");
+  Array.from(document.getElementsByClassName("cell")).forEach(function (node) {
+    node.style.backgroundColor = "white";
   });
 }
 //-------------------END Helper functions------------------
-size_btn_dom.addEventListener("click", () => {
-  size_of_pad = get_size_of_container();
+function create_grid(size_of_pad) {
+  //clear grid if already formed
+  while (container_dom.firstChild) {
+    temp_cell = document.querySelector(".cell");
+    container_dom.removeChild(temp_cell);
+  }
   let cell_width = `${Math.floor(container_width_js / size_of_pad)}px`;
+  //create cells
   for (let i = 0; i < size_of_pad ** 2; i++) {
     const new_div = document.createElement("div");
     new_div.classList.add("cell");
     new_div.style.width = cell_width;
     new_div.style.width = new_div.style.height = cell_width;
     new_div.setAttribute("id", `cell_${i + 1}`);
-    new_div.addEventListener(
-      "mousedown",
-      () => (new_div.style.backgroundColor = "black")
-    );
+    new_div.addEventListener("mouseover", () => {
+      if (win_mousedown == true) {
+        new_div.style.backgroundColor = "black";
+      }
+    });
     container_dom.appendChild(new_div);
   }
-});
-clear_btn_dom, addEventListener("click", clear());
+}
+
+clear_btn_dom.addEventListener("click", clear);
